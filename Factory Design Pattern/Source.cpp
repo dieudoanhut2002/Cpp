44e7439b3eca3cfe7b4ae4a9e492ee3f88a64223
dieudoanhut2002 <127190623+dieudoanhut2002@users.noreply.github.com>
2023-03-31 16:56:30 +0700

    31/3/23 2

---
+++ Factory Design Pattern/Source.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+
+
+class Shape {
+public:
+    virtual void draw() = 0;
+};
+class Circle : public Shape {
+public:
+    void draw() override {
+        // Triển khai để vẽ một hình tròn
+        std::cout << "Circle drawn completed!\n";
+    }
+};
+
+class Rectangle : public Shape {
+public:
+    void draw() override {
+        // Triển khai để vẽ một hình chữ nhật
+        std::cout << "Rectangle drawn completed!\n";
+    }
+};
+class ShapeFactory {
+public:
+    static Shape* createShape(const std::string& shapeType) {
+        if (shapeType == "circle") {
+            return new Circle();
+        }
+        else if (shapeType == "rectangle") {
+            return new Rectangle();
+        }
+        else {
+            return nullptr;
+        }
+    }
+};
+int main() {
+    Shape* circle = ShapeFactory::createShape("circle");
+    Shape* rectangle = ShapeFactory::createShape("rectangle");
+
+    // Sử dụng các đối tượng tạo ra
+    circle->draw();
+    rectangle->draw();
+
+    delete circle;
+    delete rectangle;
+
+    return 0;
+}
+
