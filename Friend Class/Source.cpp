11be66d14d64bd26b1f5809cf649b25b6c261318
dieudoanhut2002 <127190623+dieudoanhut2002@users.noreply.github.com>
2023-04-04 16:50:23 +0700

    4/4/2023 2

--- Friend Class/Source.cpp
+++ Friend Class/Source.cpp
@@ -4,21 +4,22 @@ class Rectangle {
 private:
 	int width;
 	int height;
-	int RectangleArea() {
-		return width * height / 2;	// Để giá trị nhỏ = 1/2 đế dễ phân biệt với 
-	}								// hàm tính diện tích trực tiếp trong Class Square
+	
 protected:
 	int RectanglePerimeter() {
 		return (width + height) * 2;
 	}
 public:
 	Rectangle(int w, int h) : width(w), height(h) {};
-	
+	int RectangleArea() {
+		return width * height / 2;	// Để giá trị nhỏ = 1/2 đế dễ phân biệt với 
+	}								// hàm tính diện tích trực tiếp trong Class Square
 	friend class Square;
+	friend class Test;
 };
 class Square: public Rectangle {
 private:
-	int side;
+	int side;	
 public:
 	Square(int x) : Rectangle(x, x), side(x) {};
 	int SquareArea() {
@@ -31,10 +32,28 @@ public:
 		return Rectangle::RectanglePerimeter();
 	}
 };
+class Test {
+public:
+	void setWidth(Rectangle& x, int w) {        //truy cập thuộc tính Private
+		x.width = w;
+	}
+	int RectanglePerimeter(Rectangle& x) {      // truy cập thuộc tính protected
+		return (x.width + x.height) * 2;
+	}
+	int Perimeter(Rectangle& x) {				// truy cập thuộc tính public
+		return x.RectanglePerimeter();
+	}
+};
 int main() {
 	Square sq(5);
 	std::cout << sq.SquareArea() << std::endl;
 	std::cout << sq.RectangleArea() << std::endl;
-	std::cout << sq.Perimeter();
+	std::cout << sq.Perimeter() << std::endl;
+	Rectangle rec(3, 4);
+	Test friendclass;
+	friendclass.setWidth(rec, 5);
+	std::cout << friendclass.RectanglePerimeter(rec) << std::endl;
+	std::cout << friendclass.Perimeter(rec) << std::endl;
+	
 	return 0;
 }
